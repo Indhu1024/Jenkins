1621529eeab4a7bc2f5a9544f9588064a114d429
Indhu1024 <83407978+Indhu1024@users.noreply.github.com>
2023-08-02 14:50:25 +0530

    Update Jenkins Job_2.groovy

--- Jenkins Job_2.groovy
+++ Jenkins Job_2.groovy
@@ -1,1 +1,29 @@
+// Jenkinsfile for SecondJob
+pipeline {
+    agent any
+
+    stages {
+        stage('Pull Docker Image') {
+            steps {
+                // Pull the Docker image
+                script {
+                    docker.withRegistry('https://hub.docker.com', 'docker-hub-credentials') {
+                        docker.image(''https://hub.docker.com/my-docker-image:latest').pull()
+                    }
+                }
+            }
+        }
+
+        stage('Run Container') {
+            steps {
+                // Run the container and print the file changes
+                script {
+                    docker.image(''https://hub.docker.com/my-docker-image:latest').inside {
+                        sh 'cat Sample.txt'
+                    }
+                }
+            }
+        }
+    }
+}
 
