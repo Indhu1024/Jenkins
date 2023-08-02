0bcc06ef02aa769417dcbfee2d7dcff50116e8ec
Indhu1024 <83407978+Indhu1024@users.noreply.github.com>
2023-08-02 18:38:32 +0530

    Update Jenkins Job_1.groovy

--- Jenkins Job_1.groovy
+++ Jenkins Job_1.groovy
@@ -10,31 +10,35 @@ pipeline {
             }
         }
         
-    
-
-        stage('Docker Build') {
-            steps {
-                // Build the Docker image
-                script {
-                     def dockerfilePath = 'C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\Jenkins Job_1'
-                     def absoluteDockerfilePath = "${WORKSPACE}/${dockerfilePath}"
-                     echo "Dockerfile Path: ${absoluteDockerfilePath}"
-                     withDockerRegistry(url:'https://hub.docker.com/', credentialsId:'docker-hub-credentials') {
-                         def dockerLoginCommand = "docker login -u indhu1024 --password-stdin https://hub.docker.com/"
-                          def dockerLoginProcess = dockerLoginCommand.execute()
-                         dockerLoginProcess.inputStream.withWriter { writer ->
-                         writer.write('Indhu@2000')
-    }
-                          dockerLoginProcess.waitFor()
+    stage('Docker Build') {
+    steps {
+        // Build the Docker image
+        script {
+            def dockerfilePath = 'C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\Jenkins Job_1'
+            def absoluteDockerfilePath = "${WORKSPACE}/${dockerfilePath}"
+            echo "Dockerfile Path: ${absoluteDockerfilePath}"
+            
+            def dockerLoginCommand = "docker login -u indhu1024 --password-stdin https://hub.docker.com/"
+            def dockerLoginProcess = dockerLoginCommand.execute()
+            dockerLoginProcess.withWriter { writer ->
+                writer.write('Indhu@2000')
+            }
+            dockerLoginProcess.waitFor()
+            
             if (dockerLoginProcess.exitValue() != 0) {
-              error("Docker login failed: ${dockerLoginProcess.err.text}")
-    }
-                        def customImage = docker.build('indhu1024/my-docker-image:latest', "-f ${absoluteDockerfilePath} .")
-                        customImage.push()
-                    }
-                }
+                error("Docker login failed: ${dockerLoginProcess.err.text}")
+            }
+            
+            withDockerRegistry(url:'https://hub.docker.com/', credentialsId:'docker-hub-credentials') {
+                def customImage = docker.build('indhu1024/my-docker-image:latest', "-f ${absoluteDockerfilePath} .")
+                customImage.push()
             }
         }
+    }
+}
+
+
+       
 
         stage('Trigger Second Job') {
             steps {
