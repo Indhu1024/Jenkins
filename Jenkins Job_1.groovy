857695058338a4d1c802892ea7319047c8038ee1
Indhu1024 <83407978+Indhu1024@users.noreply.github.com>
2023-08-02 18:14:29 +0530

    Update Jenkins Job_1.groovy

--- Jenkins Job_1.groovy
+++ Jenkins Job_1.groovy
@@ -20,6 +20,15 @@ pipeline {
                      def absoluteDockerfilePath = "${WORKSPACE}/${dockerfilePath}"
                      echo "Dockerfile Path: ${absoluteDockerfilePath}"
                      withDockerRegistry(url:'https://hub.docker.com/', credentialsId:'docker-hub-credentials') {
+                         def dockerLoginCommand = "docker login -u indhu1024 --password-stdin https://hub.docker.com/"
+                          def dockerLoginProcess = dockerLoginCommand.execute()
+                         dockerLoginProcess.inputStream.withWriter { writer ->
+                         writer.write('Indhu@2000')
+    }
+                          dockerLoginProcess.waitFor()
+            if (dockerLoginProcess.exitValue() != 0) {
+              error("Docker login failed: ${dockerLoginProcess.err.text}")
+    }
                         def customImage = docker.build('indhu1024/my-docker-image:latest', "-f ${absoluteDockerfilePath} .")
                         customImage.push()
                     }
