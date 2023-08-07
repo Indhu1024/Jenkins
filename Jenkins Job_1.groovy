8a20eea16cd4f9bd0d740b7f535cf56b505382bf
Indhu1024 <83407978+Indhu1024@users.noreply.github.com>
2023-08-07 11:12:01 +0530

    Update Jenkins Job_1.groovy

--- Jenkins Job_1.groovy
+++ Jenkins Job_1.groovy
@@ -17,16 +17,16 @@ pipeline {
                     docker.withRegistry('https://registry.hub.docker.com', 'docker-hub-credentials') {
                         docker.image('my-docker-image:newtag').build()
                     }
-            #def dockerfilePath = 'C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\Jenkins Job_1'
-            #def absoluteDockerfilePath = "${WORKSPACE}/${dockerfilePath}"
-            #echo "Dockerfile Path: ${absoluteDockerfilePath}"
-            #sh "docker login -u indhu1024 -p 'Indhu@2000' https://hub.docker.com/"
+            //def dockerfilePath = 'C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\Jenkins Job_1'
+            //def absoluteDockerfilePath = "${WORKSPACE}/${dockerfilePath}"
+            //echo "Dockerfile Path: ${absoluteDockerfilePath}"
+            //sh "docker login -u indhu1024 -p 'Indhu@2000' https://hub.docker.com/"
            
-            #withDockerRegistry(url:'https://hub.docker.com/', credentialsId:'docker-hub-credentials'){
+            //withDockerRegistry(url:'https://hub.docker.com/', credentialsId:'docker-hub-credentials'){
               
-                 #sh "docker build -t indhu1024/my-docker-image:latest -f ${absoluteDockerfilePath} ."
-                #sh "docker push indhu1024/my-docker-image:latest"
-            #hi}
+                 //sh "docker build -t indhu1024/my-docker-image:latest -f ${absoluteDockerfilePath} ."
+                //sh "docker push indhu1024/my-docker-image:latest"
+            //hi}
         }
     }
 }
