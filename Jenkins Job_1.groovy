1900d557b7f51969a472875db0bcb055a7bc1074
Indhu1024 <83407978+Indhu1024@users.noreply.github.com>
2023-08-07 11:10:14 +0530

    Update Jenkins Job_1.groovy

--- Jenkins Job_1.groovy
+++ Jenkins Job_1.groovy
@@ -14,19 +14,32 @@ pipeline {
     steps {
         // Build the Docker image
         script {
-            def dockerfilePath = 'C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\Jenkins Job_1'
-            def absoluteDockerfilePath = "${WORKSPACE}/${dockerfilePath}"
-            echo "Dockerfile Path: ${absoluteDockerfilePath}"
-            sh "docker login -u indhu1024 -p 'Indhu@2000' https://hub.docker.com/"
+                    docker.withRegistry('https://registry.hub.docker.com', 'docker-hub-credentials') {
+                        docker.image('my-docker-image:newtag').build()
+                    }
+            #def dockerfilePath = 'C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\Jenkins Job_1'
+            #def absoluteDockerfilePath = "${WORKSPACE}/${dockerfilePath}"
+            #echo "Dockerfile Path: ${absoluteDockerfilePath}"
+            #sh "docker login -u indhu1024 -p 'Indhu@2000' https://hub.docker.com/"
            
-            withDockerRegistry(url:'https://hub.docker.com/', credentialsId:'docker-hub-credentials') {
+            #withDockerRegistry(url:'https://hub.docker.com/', credentialsId:'docker-hub-credentials'){
               
-                 sh "docker build -t indhu1024/my-docker-image:latest -f ${absoluteDockerfilePath} ."
-                sh "docker push indhu1024/my-docker-image:latest"
-            }
+                 #sh "docker build -t indhu1024/my-docker-image:latest -f ${absoluteDockerfilePath} ."
+                #sh "docker push indhu1024/my-docker-image:latest"
+            #hi}
         }
     }
 }
+        stage('Push Docker Image to Docker Hub') {
+            steps {
+                // Push the built Docker image to Docker Hub
+                script {
+                    docker.withRegistry('https://registry.hub.docker.com', 'docker-hub-credentials') {
+                        docker.image('my-docker-image:newtag').push()
+                    }
+                }
+            }
+        }
         stage('Trigger Second Job') {
             steps {
                 // Trigger the second Jenkins job (provide the job name you created in Jenkins)
